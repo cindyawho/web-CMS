442710465a44b7ce1d351223a45a6f274ce11926
cindyawho <161978639+cindyawho@users.noreply.github.com>
2025-06-07 16:25:45 -0700

    Load user json data onto tkinter UI

--- createSiteFiles/GUI_load_JSON.py
+++ createSiteFiles/GUI_load_JSON.py
@@ -12,8 +12,25 @@ class GUI_load_JSON:
         self.text.pack()
 
         # Sample JSON
-        json_val = json.loads('{"a": 5, "b": 7}')
+        with open('createSiteFiles\\userFiles\\user.json', 'r') as f:
+            data = json.load(f)
+            # print(data)
+            username = data["userName"]
+            siteTitle = data["title"]
+            journalTitle = data["journalTitle"]
+            footer = data["footerDescription"]
 
-        for k in json_val:
-            self.text.insert(tk.END, '{} = {}\n'.format(k, json_val[k]))
+            bookEntry = data["bookEntries"][0]
+            date = bookEntry["date"]
+            title = bookEntry["title"]
+            author = bookEntry["date"]
+            coverImg = bookEntry["coverURL"]
+            rating = bookEntry["rating"]
+            description = bookEntry["description"]
+            spoilers = bookEntry["spoilers"]
+
+            self.text.insert(tk.END, f"{username}'s {siteTitle}\n\n")
+            self.text.insert(tk.END, f"{journalTitle}\n\n")
+            self.text.insert(tk.END, f"{date}\n {title}\n {author}\n {coverImg}\n {rating}\n {description}\n {spoilers}\n")
+            self.text.insert(tk.END, f"{footer}\n\n")
         self.text.config(state=tk.DISABLED)
--- createSiteFiles/createPage.py
+++
@@ -1,8 +0,0 @@
-import createPageUI as cui
-import GUI_load_JSON as loadJSON
-import tkinter as tk
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = loadJSON.CreatePage(root)
-    root.mainloop()
